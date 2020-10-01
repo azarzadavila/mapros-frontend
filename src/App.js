0d75ab89341fd4ec71d52d9661f6126cb90042c9
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-10-01 15:41:40 +0200

    Check authentification

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Col, Container, FormControl, Row} from "react-bootstrap";
+import {Cookies} from "react-cookie";
+
 
 // const root_url = 'http://localhost:8000/';
 const root_url = 'https://mapros.herokuapp.com/';
+
+const cookies = new Cookies();
 
 class Login extends React.Component {
     constructor(props) {
@@ -38,11 +42,14 @@ class Login extends React.Component {
             if (response.ok) {
                 return response.json();
             }
-            alert('Incorrect username or password');
+            throw new Error();
         }, networkError => {
             console.log(networkError.message);
         }).then(jsonResponse => {
             console.log(jsonResponse);
+            cookies.set('token', jsonResponse.token);
+        }).catch(error => {
+
         });
         event.preventDefault();
     }
@@ -76,13 +83,47 @@ class Login extends React.Component {
     }
 }
 
+async function checkAuth() {
+    const token = cookies.get('token');
+    if (token) {
+        return await fetch(root_url + 'auth/check/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({token: token})
+        }).then(response => {
+            return response.ok;
+        }, networkError => {
+            console.log(networkError.message);
+        });
+    }
+    return false;
+}
 
-function App() {
-    return (
-        <Container>
-            <Login/>
-        </Container>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            main: <div></div>
+        };
+        checkAuth().then(
+            response => {
+                if (response) {
+                    console.log('positive response');
+                    this.setState({main: <div>Already Connected</div>});
+                } else {
+                    console.log('negative response');
+                    this.setState({main: <Login></Login>});
+                }
+            }
+        );
+    }
+
+    render() {
+
+        return <Container>{this.state.main}</Container>
+    }
 }
 
 export default App;
