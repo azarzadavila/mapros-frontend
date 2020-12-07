a3b67bc9825cc03ab1e6d9b928df486f2ef8ce29
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-12-07 17:51:06 +0100

    Single input with dispatch

--- src/MathQuillTest.js
+++ src/MathQuillTest.js
@@ -1,15 +1,41 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
 import { addStyles, EditableMathField } from "react-mathquill";
 addStyles();
 let id = 0;
 const initialState = [{ id: 0, value: "" }];
-function reducer(state, action) {}
+function reducer(state, action) {
+  switch (action.type) {
+    case "update":
+      const newState = state.slice();
+      newState[action.index].value = action.value;
+      return newState;
+    default:
+      throw new Error();
+  }
+}
 const MathQuillTest = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const getChangeInput = (index) => {
+    return (event) => {
+      dispatch({ type: "update", index: index, value: event.target.value });
+    };
+  };
   return (
     <Container>
       <Row>
-        <InputGroup></InputGroup>
+        <InputGroup>
+          {state.map((inputObj, index) => {
+            return (
+              <input
+                key={inputObj.id}
+                type="text"
+                value={inputObj.value}
+                onChange={getChangeInput(index)}
+              />
+            );
+          })}
+        </InputGroup>
       </Row>
     </Container>
   );
