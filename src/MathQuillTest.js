69cdf6e2b91572a6bca7d30c45361d87f74a973f
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-12-07 17:56:30 +0100

    Add element

--- src/MathQuillTest.js
+++ src/MathQuillTest.js
@@ -10,6 +10,15 @@ function reducer(state, action) {
       const newState = state.slice();
       newState[action.index].value = action.value;
       return newState;
+    case "add":
+      const start = state.slice(0, action.index + 1);
+      const middle = [
+        { id: id + 1, value: "" },
+        { id: id + 2, value: "" },
+      ];
+      id += 2;
+      const end = state.slice(action.index + 1, state.length);
+      return start.concat(middle, end);
     default:
       throw new Error();
   }
@@ -19,6 +28,13 @@ const MathQuillTest = () => {
   const getChangeInput = (index) => {
     return (event) => {
       dispatch({ type: "update", index: index, value: event.target.value });
+    };
+  };
+  const getKeyDown = (index) => {
+    return (event) => {
+      if (event.ctrlKey && event.keyCode === 13) {
+        dispatch({ type: "add", index: index });
+      }
     };
   };
   return (
@@ -32,6 +48,7 @@ const MathQuillTest = () => {
                 type="text"
                 value={inputObj.value}
                 onChange={getChangeInput(index)}
+                onKeyDown={getKeyDown(index)}
               />
             );
           })}
