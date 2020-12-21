bd95fce89180c2a20d9491f962ae40bdcd815283
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-12-21 09:59:11 +0100

    Update intervals Ask/compute

--- src/Intervals.js
+++ src/Intervals.js
@@ -3,7 +3,20 @@ import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
 import { addStyles, EditableMathField } from "react-mathquill";
 
 function Intervals() {
-  return <Container />;
+  return (
+    <Container>
+      <Row>
+        <Col xs={2}>
+          <label className={"w-100"}>Ask :</label>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={2}>
+          <Button className={"w-100"}>Compute</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Intervals;
