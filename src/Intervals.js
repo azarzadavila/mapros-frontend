7e4e452303649a8bade10703a977990a74cd3af2
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-12-21 10:02:44 +0100

    MathField in intervals

--- src/Intervals.js
+++ src/Intervals.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useReducer, useState, createRef } from "react";
 import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
-import { addStyles, EditableMathField } from "react-mathquill";
+import { addStyles, EditableMathField, StaticMathField } from "react-mathquill";
 
 function Intervals() {
   return (
@@ -9,10 +9,16 @@ function Intervals() {
         <Col xs={2}>
           <label className={"w-100"}>Ask :</label>
         </Col>
+        <Col xs={10}>
+          <EditableMathField latex={"\\sqrt{x-\\gamma}"} />
+        </Col>
       </Row>
       <Row>
         <Col xs={2}>
           <Button className={"w-100"}>Compute</Button>
+        </Col>
+        <Col xs={10}>
+          <StaticMathField>{"\\frac{a}{\\beta}"}</StaticMathField>
         </Col>
       </Row>
     </Container>
