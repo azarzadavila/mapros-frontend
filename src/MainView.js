dccafa5422fd922fc6c0ec46c90ed2123b293eb0
azarzadavila <andres.zarzadavila@student.uclouvain.be>
2021-03-12 09:57:52 +0100

    ProofLine

--- src/MainView.js
+++ src/MainView.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import {
+  Alert,
+  Button,
+  Col,
+  Container,
+  Form,
+  InputGroup,
+  Row,
+} from "react-bootstrap";
 
 function HypothesisLine({ ident, text, onChange, onDelete }) {
   return (
@@ -19,10 +27,31 @@ function HypothesisLine({ ident, text, onChange, onDelete }) {
   );
 }
 
+function ProofLine({ text, onChange, onDelete }) {
+  return (
+    <Row className="mb-3">
+      <Col xs={8}>
+        <InputGroup>
+          <Form.Control value={text} onChange={onChange} />
+          <InputGroup.Append>
+            <Button onClick={onDelete}>-</Button>
+            <Button>S</Button>
+          </InputGroup.Append>
+        </InputGroup>
+      </Col>
+      <Col xs={4}>
+        <Alert variant="dark">b_n \rightarrow l</Alert>
+      </Col>
+    </Row>
+  );
+}
+
 let lastHyp = 0;
+let lastProof = 0;
 
 function MainView() {
   const [hypotheses, setHypotheses] = useState([]);
+  const [proofs, setProofs] = useState([]);
   const addHypothesis = (event) => {
     const newHypotheses = hypotheses.slice();
     newHypotheses.push({ ident: "", text: "", id: lastHyp });
@@ -37,14 +66,32 @@ function MainView() {
       setHypotheses(newHypotheses);
     };
   };
-  const handleHypothesisDelete = (index) => {
-    return (event) => {
-      const newHypothesesStart = hypotheses.slice(0, index);
-      const newHypothesesEnd = hypotheses.slice(index + 1, hypotheses.length);
-      const newHypotheses = newHypothesesStart.concat(newHypothesesEnd);
-      setHypotheses(newHypotheses);
+  const deleteFromList = (list, setList) => {
+    return (index) => {
+      return (event) => {
+        const newListStart = list.slice(0, index);
+        const newListEnd = list.slice(index + 1, list.length);
+        const newList = newListStart.concat(newListEnd);
+        setList(newList);
+      };
     };
   };
+  const handleHypothesisDelete = deleteFromList(hypotheses, setHypotheses);
+  const addProof = (event) => {
+    const newProofs = proofs.slice();
+    newProofs.push({ text: "", id: lastProof });
+    setProofs(newProofs);
+    lastProof += 1;
+  };
+  const handleProofChange = (index) => {
+    return (event) => {
+      const newProofs = proofs.slice();
+      newProofs[index] = { ...newProofs[index] };
+      newProofs[index].text = event.target.value;
+      setProofs(newProofs);
+    };
+  };
+  const handleProofDelete = deleteFromList(proofs, setProofs);
   return (
     <Container>
       <Row className="mb-3">
@@ -89,10 +136,22 @@ function MainView() {
       <Row>
         <Col xs={8}>
           Proof:
-          <Button className="ml-3 mb-3">+</Button>
+          <Button className="ml-3 mb-3" onClick={addProof}>
+            +
+          </Button>
           <Button className="ml-3 mb-3">S</Button>
         </Col>
       </Row>
+      {proofs.map((proof, index) => {
+        return (
+          <ProofLine
+            text={proof.text}
+            key={proof.id}
+            onChange={handleProofChange(index)}
+            onDelete={handleProofDelete(index)}
+          />
+        );
+      })}
     </Container>
   );
 }
